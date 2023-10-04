90162538de5cf05365b13ced77b1c59bee1168b4
glacier <glacier@node0.bighash.nestfarm-pg0.wisc.cloudlab.us>
2023-10-04 17:31:54 -0500

    add manual freq config

--- femu-scripts/set-freq.sh
+++ femu-scripts/set-freq.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-echo "use performance governor for all cpus"
-sudo cpupower frequency-set --governor performance > /dev/null
+cpupower() {
+  echo "use performance governor for all cpus"
+  sudo cpupower frequency-set --governor performance > /dev/null
 
-CSD_CPUS="$1-$2"
+  CSD_CPUS="$1-$2"
 
-CSD_FREQ="1.2GHz"
-echo "limit CSD cpus [$CSD_CPUS] to $CSD_FREQ"
-sudo cpupower -c $CSD_CPUS frequency-set -u $CSD_FREQ > /dev/null
+  CSD_FREQ="1.2GHz"
+  echo "limit CSD cpus [$CSD_CPUS] to $CSD_FREQ"
+  sudo cpupower -c $CSD_CPUS frequency-set -u $CSD_FREQ > /dev/null
+}
+
+manual() {
+  echo "use performance governor for all cpus"
+  for governor in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor; do
+    echo performance > $governor
+  done
+
+  echo "limit CSD cpus [$CSD_CPUS] to $CSD_FREQ"
+  for ((i=$1; i<=$2; i++)); do
+    echo 1200000 | sudo tee /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq
+  done
+}
